97a73153fb274b6594450e43d5283f26b81c9a19
QYD720 <QYD720@hotmail.com>
2019-04-30 16:58:44 +0800

    pythondemo

--- pythondemo/pythondemohello.py
+++ pythondemo/pythondemohello.py
@@ -7,7 +7,7 @@ from itchat.content import *
 def text_reply(msg):
     match = re.search('hello',msg['Text'])
     if match:
-        itchat.send(('嘿嘿嘿,这是一个demo'),msg['FromUserName'])
+        itchat.send(('嘿嘿嘿,这是一个DDemo'),msg['FromUserName'])
 
 itchat.auto_login(enableCmdQR=True)
 itchat.run()
